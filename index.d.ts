6f1850e534fb1f292d12094b889e94375c04dbb8
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2024-02-07 09:07:51 +0200

    Add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import { Schema } from "mongoose";
+declare namespace wrapCallback {
+  interface Options {
+    mode?: 'scope' | 'emit';
+  }
+}
+declare function wrapCallback(schema: Schema, options?: wrapCallback.Options): void;
+
+export = wrapCallback;
